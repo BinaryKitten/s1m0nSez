b54d517da70b9db56081b8e8c2aa2d792e2e7104
Kathryn Reeve <Kathryn@binaryKitten.com>
2013-08-12 22:19:13 +0100

    update practically complete game

--- Simon.ino
+++ Simon.ino
@@ -1,8 +1,8 @@
+//#include <Tone.h>
+
 #include <Button.h>
 
 #include "pitches.h"
-
-#define SIZEOF_ARRAY(x)  (sizeof (x) / sizeof (x[0]) )
 
 int simon[30] = {};
 int pos = 0;
@@ -15,25 +15,14 @@ Button button3 = Button(5, BUTTON_PULLDOWN);
 Button button4 = Button(4, BUTTON_PULLDOWN);
 
 const int pinCount = sizeof(ledPins) / sizeof(int);
-int ledPin = 0;
-int buttonState = 0; 
+int ledPin = 0; 
 
-int pinMelodies[][2] = {
-  {NOTE_C4, 8},
-  {NOTE_G3, 8},
-  {NOTE_A3, 8},
-  {NOTE_B3, 8},
-}; 
-
-int funeral[] = {
-  NOTE_G4, NOTE_G4, NOTE_C5, NOTE_G4, NOTE_C5, NOTE_E5, NOTE_G4, NOTE_C5, NOTE_E5, NOTE_G4, NOTE_C5, NOTE_E5, NOTE_G4, NOTE_C5, NOTE_E5, NOTE_C5, NOTE_E5, NOTE_G5, NOTE_E5, NOTE_C5, NOTE_G4, NOTE_G4, NOTE_G4, NOTE_C5};
-
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-int funeralDurations[] = {
-  2,2,1,2,2,1,2,2,2,2,2,2,2,2,1,2,2,1,2,2,1,2,2,1};
-
-int imperial[][2] = {
-//  G G G Eb Bb G Eb Bb G(up above tuning C) D D D Eb (back down) Bb G Eb Bb G G low GG high G F# F E Eb E Ab C# C B Bb A Bb Eb F# Bb G BbD G GG G F# F E Eb E Ab C# C B Bb A Bb Eb Bb GEb Bb G
+int pinMelodies[5][2] = {
+  {NOTE_B4, 8},
+  {NOTE_C5, 8},
+  {NOTE_A4, 8},
+  {NOTE_E4, 8},
+  {NOTE_G2, 4}
 };
 
 void setup() {
@@ -60,14 +49,13 @@ void loop() {
       showMe(buttonPressed);
       if (buttonPressed != pinId) {
         Serial.println("Failed on level: " + String(pos) + " by pressing " + String(buttonPressed) + " when you should have pressed " + String(pinId));
-        playMelody(funeral, funeralDurations);
+//        playMelody(funeral, funeralDurations);
         showError();
         pickSimon();
         playStartSound();
         pos = -1;
       }
       Serial.print(String(buttonPressed) + "  ");
-      delay(500);
     }
     Serial.println("");
     pos += 1;
@@ -89,14 +77,8 @@ void showMe(int pinId) {
 }
 
 void showError() {
-  int len = (sizeof(funeral) / sizeof(int));
-  Serial.println("size: " + String(sizeof(funeral)) + " - " + String(len));
-  for (int thisNote = 0; thisNote < len; thisNote++) {
-    int note = funeral[thisNote];
-    int duration = funeralDurations[thisNote];
-    playNote(note, duration);
-  }
-  
+  int note = pinMelodies[4][0];
+  int duration = pinMelodies[4][1];
   
   int state = LOW;
   for(int flashy = 0;flashy < 5; flashy++) {
@@ -108,9 +90,8 @@ void showError() {
     } else {
       state = LOW;
     }
-    delay(500);
+    playNote(note, duration);
   }
-//  delay(1000);
   
 }
 
@@ -126,6 +107,7 @@ void pickSimon() {
   // different seed numbers each time the sketch runs.
   // randomSeed() will then shuffle the random function.
   randomSeed(analogRead(0));
+  int newPin = 1;
   for(int i = 0;i < 30; i++) {
     if (i < pinCount) {
       digitalWrite(ledPins[i], LOW);
@@ -142,10 +124,6 @@ void playSequenceTo(int level) {
   }
 }
 
-void playMelody(int melody[], int durations[]) {
-  
-}
-
 void playNote(int note, int duration) {
   int noteDuration = 1000/duration;
   tone(8, note, noteDuration);
@@ -154,7 +132,7 @@ void playNote(int note, int duration) {
     //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
   int pauseBetweenNotes = noteDuration * 1.01;
   delay(pauseBetweenNotes);
-  
+//  
   noTone(8);
 }
 
